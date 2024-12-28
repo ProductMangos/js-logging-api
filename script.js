7a33d2d616788377631158fa6c5e1cc95e68ca8d
jordancuadrado <jac59155@gmail.com>
2024-12-27 21:49:15 -0500

    added the other log values

--- script.js
+++ script.js
@@ -23,6 +23,36 @@ app.post('/trace', (req, res) => {
     res.send(response.message);
 })
 
+app.post('/debug', (req, res) => {
+    const response = req.body;
+    log.debug(response.message);
+    res.send(response.message);
+})
+
+app.post('/info', (req, res) => {
+    const response = req.body;
+    log.info(response.message);
+    res.send(response.message);
+})
+
+app.post('/warn', (req, res) => {
+    const response = req.body;
+    log.warn(response.message);
+    res.send(response.message);
+})
+
+app.post('/error', (req, res) => {
+    const response = req.body;
+    log.error(response.message);
+    res.send(response.message);
+})
+
+app.post('/fatal', (req, res) => {
+    const response = req.body;
+    log.fatal(response.message);
+    res.send(response.message);
+})
+
 app.listen(9999, () => {
     console.log('Server started on port 9999');
 });
