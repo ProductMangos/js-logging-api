555d8ad67fc62e6bc973860abc176fd5c92aadfe
jordancuadrado <jac59155@gmail.com>
2024-12-27 21:48:34 -0500

    added logging trace

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const bunyan = require('bunyan');
+
+const log = bunyan.createLogger({
+    name: 'myapp',
+    streams: [
+        {
+            level: 'trace',
+            path: 'logging.log'
+        }
+    ]
+})
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.post('/trace', (req, res) => {
+    const response = req.body;
+    log.trace(response.message);
+    res.send(response.message);
+})
+
+app.listen(9999, () => {
+    console.log('Server started on port 9999');
 });
