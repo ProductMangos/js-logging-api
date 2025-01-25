036646b88205a7b37a1788390be585908753b83a
jordancuadrado <jac59155@gmail.com>
2025-01-24 21:19:49 -0500

    js logging api - custom logger

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const readline = require('readline');
 const bunyan = require('bunyan');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
+const os = require('os');
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
@@ -14,15 +15,57 @@ const limiter = rateLimit({
     message: 'Too many requests from this IP, please try again later.'
   });
 
-const log = bunyan.createLogger({
-    name: 'myapp',
-    streams: [
-        {
-            level: 'trace',
-            path: 'logging.log'
-        }
-    ]
-})
+const trackingAlerts = (appName, msg) => {
+  const currentDate = new Date().toISOString();
+
+  const alerts = {
+    trace: { 
+      appName: appName,
+      name: os.hostname(), 
+      msg: msg, 
+      time: currentDate, 
+      type: 'trace',
+      level: 10
+    },
+    info: { 
+      appName: appName,
+      name: os.hostname(), 
+      msg: msg, 
+      time: currentDate, 
+      type: 'info',
+      level: 20
+    },
+    warn: {
+      appName: appName,
+      name: os.hostname(),
+      msg: msg,
+      time: currentDate,
+      type: 'warn',
+      level: 30
+    },
+    error: {
+      appName: appName,
+      name: os.hostname(),
+      msg: msg,
+      time: currentDate,
+      type: 'error',
+      level: 40
+    },
+    fatal: {
+      appName: appName,
+      name: os.hostname(),
+      msg: msg,
+      time: currentDate,
+      type: 'fatal',
+      level: 50
+    }
+  };
+
+  return alerts;
+};
+
+
+
 const app = express();
 app.use(express.json())
 app.use(cors())
@@ -31,44 +74,50 @@ app.get('/', (req, res) => {
     res.send('API is running!');
 });
 
+app.get('/test', (req, res) => {
+  const response = req.body;
+  const logData = JSON.stringify(trackingAlerts(response.appName, response.message).info, null, 2);
+  fs.writeFileSync('test.log', logData + ',\n', { flag: 'a' });
+  res.send('Log appended to test.log');
+});
+
 app.post('/trace', limiter,(req, res) => {
     const response = req.body;
-    log.trace(response.message);
-    res.send(response.message);
-})
-
-app.post('/debug', limiter, (req, res) => {
-    const response = req.body;
-    log.debug(response.message);
-    res.send(response.message);
+    const logData = JSON.stringify(trackingAlerts(response.appName, response.message).trace, null, 2);
+    fs.writeFileSync('test.log', logData + ',\n', { flag: 'a' });
+    res.send("trace logged");
 })
 
 app.post('/info', limiter, (req, res) => {
     const response = req.body;
-    log.info(response.message);
-    res.send(response.message);
+    const logData = JSON.stringify(trackingAlerts(response.appName, response.message).info, null, 2);
+    fs.writeFileSync('test.log', logData + ',\n', { flag: 'a' });
+    res.send("info logged");
 })
 
 app.post('/warn', limiter, (req, res) => {
     const response = req.body;
-    log.warn(response.message);
-    res.send(response.message);
+    const logData = JSON.stringify(trackingAlerts(response.appName, response.message).warn, null, 2);
+    fs.writeFileSync('test.log', logData + ',\n', { flag: 'a' });
+    res.send("warn logged");
 })
 
 app.post('/error', limiter, (req, res) => {
     const response = req.body;
-    log.error(response.message);
-    res.send(response.message);
+    const logData = JSON.stringify(trackingAlerts(response.appName, response.message).error, null, 2);
+    fs.writeFileSync('test.log', logData + ',\n', { flag: 'a' });
+    res.send("error logged");
 })
 
 app.post('/fatal', limiter, (req, res) => {
     const response = req.body;
-    log.fatal(response.message);
-    res.send(response.message);
+    const logData = JSON.stringify(trackingAlerts(response.appName, response.message).fatal, null, 2);
+    fs.writeFileSync('test.log', logData + ',\n', { flag: 'a' });
+    res.send("fatal logged");
 })
 
 app.get('/getlogs', (req, res) => {
-    const logFilePath = path.join(__dirname, 'logging.log');
+    const logFilePath = path.join(__dirname, 'test.log');
 
     const stream = fs.createReadStream(logFilePath, { encoding: 'utf8' });
     const rl = readline.createInterface({ input: stream });
